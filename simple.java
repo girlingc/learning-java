d57c263c8aca720d95d03e21619ef9a9ee8688b4
Christian Girling <girling.christian@gmail.com>
2022-05-11 23:42:54 -0700

    Fix errors in code

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
-class Simple{
-  public void main(String args[]) {
-    System.out.printin("Hello Java")
-  }
-}
+class Simple{  
+  public static void main(String args[]){  
+   System.out.println("Hello Java");  
+  }  
+}  
