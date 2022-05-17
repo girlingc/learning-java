d5b54a5d869b5da0d60382d3e403f96af24649cb
Christian Girling <girling.christian@gmail.com>
2022-05-16 17:52:41 -0700

    Add more operators

--- OperatorExample.java
+++ OperatorExample.java
@@ -1,9 +1,8 @@
 class OperatorExample{  
   public static void main(String[] args){  
-  int x=10;  
-  System.out.println(x++);
-  System.out.println(++x);
-  System.out.println(x--);
-  System.out.println(--x);
+  int a=10;  
+  int b=10;
+  System.out.println(a++ + ++a);
+  System.out.println(b++ + b++);
   }
 }  
