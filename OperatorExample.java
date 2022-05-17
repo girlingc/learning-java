142a48e927d10fb1acc3bd828a8ad16cc04d89ef
Christian Girling <girling.christian@gmail.com>
2022-05-16 17:48:47 -0700

    Compile Operator Example

--- OperatorExample.java
+++ OperatorExample.java
@@ -1,9 +1,9 @@
-public class OperatorExample{
-  public static void main(String args[]){
-    int x=10;
-    System.out.println(x++);
-    System.out.println(++x);
-    System.out.println(x--);
-    System.out.println(--x);
+class OperatorExample{  
+  public static void main(String[] args){  
+  int x=10;  
+  System.out.println(x++);
+  System.out.println(++x);
+  System.out.println(x--);
+  System.out.println(--x);
   }
-}
+}  
