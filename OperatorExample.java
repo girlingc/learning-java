faa2f44f05da5b04fbeddff3a05ae0673d095883
Christian Girling <girling.christian@gmail.com>
2022-05-16 17:59:56 -0700

    Use right shift operator

--- OperatorExample.java
+++ OperatorExample.java
@@ -2,10 +2,8 @@ class OperatorExample{
   public static void main(String[] args){  
   int a=10;  
   int b=20;
-  int c=15;
-  System.out.println(a<<2);
-  System.out.println(a<<3);
-  System.out.println(b<<2);
-  System.out.println(c<<4);
+  System.out.println(a>>2);
+  System.out.println(b>>2);
+  System.out.println(b>>3);
   }
 }  
