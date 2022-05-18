0c92a2bdd4255920d2ce19c0d780631a0bb5a56f
Christian Girling <girling.christian@gmail.com>
2022-05-17 22:22:47 -0700

    Add exclamation point

--- java-tutorial/LearnJava.java
+++ java-tutorial/LearnJava.java
@@ -1,6 +1,10 @@
 public class LearnJava {
   public static void main(String[] args) {
     String name = "Christian";
-    System.out.println(name.toUpperCase());
+    addExclamationPoint(name);
+  }
+
+  public static void addExclamationPoint(String s) {
+    System.out.println(s + "!");
   }
 }
