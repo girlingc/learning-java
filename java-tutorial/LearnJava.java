06400585221f7557f9f1bc4623c8440223d6ea0e
Christian Girling <girling.christian@gmail.com>
2022-05-17 22:23:58 -0700

    Return method

--- java-tutorial/LearnJava.java
+++ java-tutorial/LearnJava.java
@@ -1,10 +1,11 @@
 public class LearnJava {
   public static void main(String[] args) {
     String name = "Christian";
-    addExclamationPoint(name);
+    String exclaim = addExclamationPoint(name);
+    System.out.println(exclaim);
   }
 
-  public static void addExclamationPoint(String s) {
-    System.out.println(s + "!");
+  public static String addExclamationPoint(String s) {
+    return s + "!";
   }
 }
