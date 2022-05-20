c8e0be9d1f82803c260ce33de96e217ba60e99b9
Christian Girling <girling.christian@gmail.com>
2022-05-19 23:54:41 -0700

    Change if else statement to else if statement

--- java-tutorial/Student.java
+++ java-tutorial/Student.java
@@ -1,11 +1,13 @@
 public class Student {    
   public static void main(String[] args) {    
-    int x = 1;    
-    int y = 8;    
-    if(x+y < 10) {  
+    int x = 2;    
+    int y = 18;    
+    if (x + y < 10) {  
       System.out.println("x + y is less than 10");    
+    } else if (x + y < 20){
+      System.out.println("x + y is less than 20 but greater than 10");  
     } else {
-      System.out.println("x + y is greater than 10");  
+      System.out.println("x + y is greater than 20"); 
     }
   }      
 }     
