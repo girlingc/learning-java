6158441c349f17ed3310cb93fb218f5bee9d4d84
Christian Girling <girling.christian@gmail.com>
2022-05-22 21:34:25 -0700

    Formatted file

--- MortgageCalc.java
+++ MortgageCalc.java
@@ -3,36 +3,40 @@ import java.util.Scanner;
 
 public class MortgageCalc {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    // constant values
+    final byte PERCENT = 100;
+    final byte MONTHS_IN_YEAR = 12;
 
-        // constant values
-        final byte PERCENT = 100;
-        final byte MONTHS_IN_YEAR = 12;
+    // declaring scanner object to accept user input
+    Scanner userInput = new Scanner(System.in);
 
-        // declaring scanner object to accept user input
-        Scanner userInput = new Scanner(System.in);
+    // asking for user input and giving back appropriate values
+    System.out.print("Principal Amount: ");
+    int principal = userInput.nextInt();
 
-        // asking for user input and giving back appropriate values
-        System.out.print("Principal Amount: ");
-        int principal = userInput.nextInt();
+    System.out.print("Annual Interest Rate: ");
+    float monthlyInterestRate =
+      ((userInput.nextFloat() / PERCENT) / MONTHS_IN_YEAR);
+    System.out.println("Monthly Interest Rate: " + monthlyInterestRate);
 
-        System.out.print("Annual Interest Rate: ");
-        float monthlyInterestRate = ((userInput.nextFloat() / PERCENT) / MONTHS_IN_YEAR);
-        System.out.println("Monthly Interest Rate: " + monthlyInterestRate);
+    System.out.print("Period (Years): ");
+    short period = (short) (userInput.nextShort() * MONTHS_IN_YEAR); // stores 'period' in months
+    System.out.println("Period (Months): " + period);
 
-        System.out.print("Period (Years): ");
-        short period= (short) (userInput.nextShort() * MONTHS_IN_YEAR); // stores 'period' in months
-        System.out.println("Period (Months): " + period);
+    userInput.close(); // closing the scanner object
 
-        userInput.close(); // closing the scanner object
+    // formula for calculating mortgage implemented in java
+    double mortgage =
+      (
+        principal *
+        (monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, period))) /
+        ((Math.pow(1 + monthlyInterestRate, period)) - 1)
+      );
 
-        // formula for calculating mortgage implemented in java
-        double mortgage = (principal * (monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, period)))
-                / ((Math.pow(1 + monthlyInterestRate, period)) - 1));
-
-        // prints the properly formatted mortgage value in currency
-        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
-
-
-    }
+    // prints the properly formatted mortgage value in currency
+    System.out.println(
+      "Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage)
+    );
+  }
 }
