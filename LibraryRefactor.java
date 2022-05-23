c048e93db2c78eba5dc581573a0506032772a48f
Christian Girling <girling.christian@gmail.com>
2022-05-23 00:11:10 -0700

    Initial commit LibraryRefactor

---
+++ LibraryRefactor.java
@@ -0,0 +1,16 @@
+public class LibraryRefactor {
+
+  private String id;
+  private String name;
+  private String artist;
+  private String album;
+
+  public Track(
+    final String id,
+    final String name,
+    final String artist,
+    final String album
+  ) {
+    
+  }
+}
