01380b54e2473ebc2464de426beda283248563fc
Christian Girling <girling.christian@gmail.com>
2022-05-23 00:23:13 -0700

    Add get track info method

--- LibraryRefactor.java
+++ LibraryRefactor.java
@@ -11,6 +11,14 @@ public class LibraryRefactor {
     final String artist,
     final String album
   ) {
-    
+    this.id = id;
+    this.name = name;
+    this.artist = artist;
+    this.album = album;
+  }
+  public String getTrackInfo() {
+    return this.id + this.name + this.artist + this.album;
   }
 }
+final Track t01 = ("t01", "Code Monkey", "Jonathon Coulton", "Thing a Week Three");
+track.getTrackInfo();
